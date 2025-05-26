9fee76de6c940008bbdfd6153c3d8f9fc36d2ca9
naliya643 <hilaliyah643@gmail.com>
2025-05-26 12:20:32 +0700

    modul 1

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+app.use(express.json());
+app.use('/api', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
